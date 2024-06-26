bd41b8f4405155753007b43aeb4a089b3a1c00aa
JoseTrujillSilv <josetrujillosilva@gmail.com>
2024-06-26 16:47:35 +0000

    nuevos cambios

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,18 +1,46 @@
-<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+<style>
+    *{
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
+    font-family: Arial, Helvetica, sans-serif;
+}
+.container{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+}
 
+.row{
+    display: grid;
+    grid-template-columns: 2fr 2fr;
+    gap: 20px;
+}
+
+.col-50{
+    width: 300px;
+}
+form{
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    gap: 20px;
+}
+</style>
 
 <div class="container">
     <div class="row">
         <div class="col-50">
-            <form action="">
+            <form action="/login">
                 <div>
                     <h2>Logeate:</h2>
                 </div>
                 <div>
-                    <input type="text" name="dniLogin" id="dniLogin">
+                    <input type="text" name="dniLogin" id="dniLogin" placeholder="dni...">
                 </div>
                 <div>
-                    <input type="text" name="nameLogin" id="nameLogin">
+                    <input type="text" name="nameLogin" id="nameLogin" placeholder="name...">
                 </div>
                 <div>
                     <button>Enviar</button>
@@ -20,24 +48,26 @@
             </form>
         </div>
         <div class="col-50">
-            <div>
-                <h2>Registrate</h2>
-            </div>
-            <div>
-                <input type="text" name="dniReg" id="dniReg">
-            </div>
-            <div>
-                <input type="text" name="nameReg" id="nameReg">
-            </div>
-            <div>
-                <input type="text" name="last_nameReg" id="last_nameReg">
-            </div>
-            <div>
-                <input type="text" name="edadReg" id="edadReg">
-            </div>
-            <div>
-                <button>Enviar</button>
+                <form action="/reg">
+                    <div>
+                        <h2>Registrate</h2>
+                    </div>
+                    <div>
+                        <input type="text" name="nameReg" id="nameReg" placeholder="name...">
+                    </div>
+                    <div>
+                        <input type="text" name="last_nameReg" id="last_nameReg" placeholder="last_name...">
+                    </div>
+                    <div>
+                        <input type="number" name="edadReg" id="edadReg" placeholder="30 años">
+                    </div>
+                    <div>
+                     <input type="number" name="cuentaReg" id="cuentaReg" placeholder="nº Cuenta">
+                    </div>
+                    <div>
+                        <button>Enviar</button>
+                    </div>
+                </form>
             </div>
         </div>
-    </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
-
-Route::get('/', function () {
-    return view('index');
-});
-
