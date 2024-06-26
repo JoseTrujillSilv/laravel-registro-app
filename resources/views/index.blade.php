7d64106c61e4a43ae60784b991874ece099c633c
JoseTrujillSilv <josetrujillosilva@gmail.com>
2024-06-26 18:17:46 +0200

    cambios index vistas

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,42 @@
-<div>
-    <!-- When there is no desire, all things are at peace. - Laozi -->
+<link rel="stylesheet" href="{!! asset('css/app.css') !!}">
+
+<div class="container">
+    <div class="row">
+        <div class="col-50">
+            <form action="">
+                <div>
+                    <h2>Logeate:</h2>
+                </div>
+                <div>
+                    <input type="text" name="dniLogin" id="dniLogin">
+                </div>
+                <div>
+                    <input type="text" name="nameLogin" id="nameLogin">
+                </div>
+                <div>
+                    <button>Enviar</button>
+                </div>
+            </form>
+        </div>
+        <div class="col-50">
+            <div>
+                <h2>Registrate</h2>
+            </div>
+            <div>
+                <input type="text" name="dniReg" id="dniReg">
+            </div>
+            <div>
+                <input type="text" name="nameReg" id="nameReg">
+            </div>
+            <div>
+                <input type="text" name="last_nameReg" id="last_nameReg">
+            </div>
+            <div>
+                <input type="text" name="edadReg" id="edadReg">
+            </div>
+            <div>
+                <button>Enviar</button>
+            </div>
+        </div>
+    </div>
 </div>
